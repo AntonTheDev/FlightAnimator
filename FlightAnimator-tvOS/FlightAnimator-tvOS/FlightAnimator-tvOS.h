364cb8c973d02169e1eb9dd93a6ce1b2aa689cdb
Anton Doudarev <antonthedev@gmail.com>
2016-07-07 18:59:09 -0700

    Added TvOS Target

---
+++ FlightAnimator-tvOS/FlightAnimator-tvOS/FlightAnimator-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  FlightAnimator-tvOS.h
+//  FlightAnimator-tvOS
+//
+//  Created by Anton on 7/7/16.
+//  Copyright © 2016 Anton Doudarev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlightAnimator-tvOS.
+FOUNDATION_EXPORT double FlightAnimator_tvOSVersionNumber;
+
+//! Project version string for FlightAnimator-tvOS.
+FOUNDATION_EXPORT const unsigned char FlightAnimator_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlightAnimator_tvOS/PublicHeader.h>
+
+
